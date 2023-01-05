2ff7979b8a8f7a851d377125d27cd052c3b94e61
aaronrod3 <aaronrod@me.com>
2023-01-04 19:16:15 -0500

    Prep2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade? ");
+        string gradeInput = Console.ReadLine();
+        int grade = int.Parse(gradeInput);
+
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade > 70)
+        {
+            letter = "C";
+        }
+        else if (grade > 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+            if ()
+
+        Console.WriteLine($"Your grade is {letter}.");
+
+        if (grade > 60)
+        {
+            Console.WriteLine("Congrats! You passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("You failed!");
+        }
+
     }
 }
