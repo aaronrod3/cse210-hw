cdac9ff24c4c0b4bea5f8de02b77140f1edb8012
Aaron <aaronrod@me.com>
2023-01-09 21:20:04 -0500

    prep3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+
+        int numberGuesses = 0;
+
+
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("What's your guess: ");
+            guess = int.Parse(Console.ReadLine());
+
+            numberGuesses = numberGuesses + 1;
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                Console.Write ($"Number Guesses = {numberGuesses}");
+            }
+        }
+
+
+    
+    
     }
+
 }
