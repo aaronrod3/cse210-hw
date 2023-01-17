3aa6ef4799f87ec04a928a7c9d4c7b53fb5ae489
Aaron <aaronrod@me.com>
2023-01-16 19:29:51 -0500

    lesson2 completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._parentCompany = "Microsoft";
+        job1._startYear = 2010;
+        job1._endYear = 2020;
+
+        Resume myResume = new Resume();
+        myResume._name = "Aaron Rodriguez";
+
+        myResume._jobs.Add(job1);
+        myResume.Display();
+
     }
 }
