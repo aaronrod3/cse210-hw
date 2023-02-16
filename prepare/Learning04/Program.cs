4be4a80a51ba9f2a499fc31765ed3bd0d2c8e72c
Aaron <aaronrod@me.com>
2023-02-15 20:51:14 -0500

    finished learn 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Aaron Rodriguez", "Math");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment math1 = new MathAssignment("Aaron Rodriguez","Math", "7.3", "8-19");
+        Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssignment writing1 = new WritingAssignment("A R", "History", "World History");
+        Console.WriteLine(writing1.GetWritingInformation());
     }
 }
